8e6ed66926bc713a099b6b507170260dc3c17b8f
Kevin Lee <kevinlee3627@gmail.com>
2023-04-10 12:56:29 -0500

    recursive settimoeut

--- src/main.ts
+++ src/main.ts
@@ -11,76 +11,8 @@ function msToTime(ms: number): string {
   return `${minutes}:${secondsStr}`;
 }
 
-function showScreen(playback: Playback): void {
-  const screen = b.screen({ smartCSR: true, autoPadding: true });
-
-  screen.key(['escape', 'q', 'C-c'], function (ch, key) {
-    return process.exit(0);
-  });
-
-  screen.title = 'TEST!';
-
-  const box = b.box({
-    parent: screen,
-    width: '100%',
-    tags: true,
-  });
-
-  const songTitle = playback.item.name;
-  const songArtist = playback.item.album.artists.map((artist) => artist.name).join(', ');
-  const songInfo = b.text({
-    top: '100%-2',
-    parent: box,
-    content: `PLAYING: {bold}${songTitle}{/bold} by ${songArtist}`,
-    tags: true,
-  });
-  box.append(songInfo);
-
-  const progressBox = b.box({
-    parent: box,
-    top: '100%-1',
-    width: '100%',
-  });
-
-  const progressBar = b.progressbar({
-    parent: progressBox,
-    filled: (playback.progress_ms / playback.item.duration_ms) * 100,
-    left: 'center',
-    width: '100%-12',
-    orientation: 'horizontal',
-    pch: '█',
-  });
-  progressBox.append(progressBar);
-
-  const totalTime = b.text({
-    parent: progressBox,
-    content: msToTime(playback.item.duration_ms),
-    right: 0,
-  });
-  progressBox.append(totalTime);
-
-  const timeElapsed = b.text({
-    parent: progressBox,
-    content: msToTime(playback.progress_ms),
-    tags: true,
-  });
-  progressBox.append(timeElapsed);
-
-  box.append(progressBox);
-
-  screen.append(box);
-
-  let progress = playback.progress_ms;
-  // TODO: Once the progress tracked here reaches 100%, check the playback status again
-  setInterval(() => {
-    if (playback.is_playing) {
-      progress += 1000;
-      const percentage = (progress / playback.item.duration_ms) * 100;
-      progressBar.setProgress(percentage);
-      timeElapsed.setContent(msToTime(progress));
-    }
-    screen.render();
-  }, 1000);
+class App {
+  screen = b.screen({ smartCSR: true, autoPadding: true });
 }
 
 function showGrid(playback: Playback): void {
@@ -121,15 +53,16 @@ function showGrid(playback: Playback): void {
   });
   songBox.append(totalTime);
 
-  let progress = playback.progress_ms;
+  void refreshScreen(
+    screen,
+    playback,
+    playback.progress_ms,
+    playback.item.duration_ms,
+    progressBar,
+    timeElapsed
+  );
   // TODO: Once the progress tracked here reaches 100%, check the playback status again
-  setInterval(() => {
-    if (playback.is_playing) {
-      progress += 1000;
-      updateProgress(progress, playback.item.duration_ms, progressBar, timeElapsed);
-    }
-    screen.render();
-  }, 1000);
+  setTimeout(() => {}, 1000);
 
   screen.key(['escape', 'q', 'C-c'], function (ch, key) {
     return process.exit(0);
@@ -137,14 +70,39 @@ function showGrid(playback: Playback): void {
   screen.render();
 }
 
+async function refreshScreen(
+  screen: b.Widgets.Screen,
+  playback: Playback,
+  progress: number,
+  duration: number,
+  progressBar: b.Widgets.ProgressBarElement,
+  timeElapsed: b.Widgets.TextElement
+): Promise<void> {
+  if (playback.is_playing) {
+    progress += 1000;
+    if (progress >= duration + 1000) {
+      const spotify = new Spotify();
+      await spotify.getToken();
+      playback = await spotify.getPlaybackState();
+      progress = playback.progress_ms;
+      duration = playback.item.duration_ms;
+    }
+    updateProgress(progress, duration, progressBar, timeElapsed);
+  }
+  setTimeout(() => {
+    void refreshScreen(screen, playback, progress, duration, progressBar, timeElapsed);
+  }, 1000);
+  screen.render();
+}
+
 function updateProgress(
   progress: number,
   duration: number,
   progressBar: b.Widgets.ProgressBarElement,
-  timeText: b.Widgets.TextElement
+  timeElapsed: b.Widgets.TextElement
 ): void {
   progressBar.setProgress((progress / duration) * 100);
-  timeText.setContent(msToTime(progress));
+  timeElapsed.setContent(msToTime(progress));
 }
 
 async function main(): Promise<void> {
