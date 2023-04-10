d903ef8023050981501b3fc05651384cf32e892e
Kevin Lee <kevinlee3627@gmail.com>
2023-04-10 12:41:49 -0500

    move everything to new grid

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import * as blessed from 'blessed';
+import * as b from 'blessed';
+import bc from 'blessed-contrib';
 import type { Playback } from './types';
 import { Spotify } from './spotify';
 
@@ -11,7 +12,7 @@ function msToTime(ms: number): string {
 }
 
 function showScreen(playback: Playback): void {
-  const screen = blessed.screen({ smartCSR: true, autoPadding: true });
+  const screen = b.screen({ smartCSR: true, autoPadding: true });
 
   screen.key(['escape', 'q', 'C-c'], function (ch, key) {
     return process.exit(0);
@@ -19,7 +20,7 @@ function showScreen(playback: Playback): void {
 
   screen.title = 'TEST!';
 
-  const box = blessed.box({
+  const box = b.box({
     parent: screen,
     width: '100%',
     tags: true,
@@ -27,7 +28,7 @@ function showScreen(playback: Playback): void {
 
   const songTitle = playback.item.name;
   const songArtist = playback.item.album.artists.map((artist) => artist.name).join(', ');
-  const songInfo = blessed.text({
+  const songInfo = b.text({
     top: '100%-2',
     parent: box,
     content: `PLAYING: {bold}${songTitle}{/bold} by ${songArtist}`,
@@ -35,32 +36,30 @@ function showScreen(playback: Playback): void {
   });
   box.append(songInfo);
 
-  const progressBox = blessed.box({
+  const progressBox = b.box({
     parent: box,
     top: '100%-1',
     width: '100%',
-    height: '70',
   });
 
-  const progressBar = blessed.progressbar({
+  const progressBar = b.progressbar({
     parent: progressBox,
     filled: (playback.progress_ms / playback.item.duration_ms) * 100,
     left: 'center',
     width: '100%-12',
-    height: 1,
     orientation: 'horizontal',
     pch: '█',
   });
   progressBox.append(progressBar);
 
-  const totalTime = blessed.text({
+  const totalTime = b.text({
     parent: progressBox,
     content: msToTime(playback.item.duration_ms),
     right: 0,
   });
   progressBox.append(totalTime);
 
-  const timeElapsed = blessed.text({
+  const timeElapsed = b.text({
     parent: progressBox,
     content: msToTime(playback.progress_ms),
     tags: true,
@@ -84,11 +83,76 @@ function showScreen(playback: Playback): void {
   }, 1000);
 }
 
+function showGrid(playback: Playback): void {
+  const screen = b.screen({ smartCSR: true, autoPadding: true });
+  const gridHeight = 48;
+  const gridWidth = 48;
+  const grid = new bc.grid({ rows: gridHeight, cols: gridWidth, screen });
+
+  const songTitle = playback.item.name;
+  const songArtist = playback.item.album.artists.map((artist) => artist.name).join(', ');
+  const albumName = playback.item.album.name;
+  const albumYear = playback.item.album.release_date.split('-')[0];
+  const songBox = grid.set(gridHeight - 3, 0, 3, gridWidth, b.box, {
+    label: `{bold}${songTitle}{/bold} by ${songArtist} | ${albumName} (${albumYear})`,
+    tags: true,
+  });
+
+  const progressBar = b.progressbar({
+    filled: (playback.progress_ms / playback.item.duration_ms) * 100,
+    left: 'center',
+    width: '100%-12',
+    orientation: 'horizontal',
+    pch: '█',
+  });
+
+  songBox.append(progressBar);
+
+  const timeElapsed = b.text({
+    content: msToTime(playback.progress_ms),
+    left: '0',
+    tags: true,
+  });
+  songBox.append(timeElapsed);
+
+  const totalTime = b.text({
+    content: msToTime(playback.item.duration_ms),
+    left: '100%-6',
+  });
+  songBox.append(totalTime);
+
+  let progress = playback.progress_ms;
+  // TODO: Once the progress tracked here reaches 100%, check the playback status again
+  setInterval(() => {
+    if (playback.is_playing) {
+      progress += 1000;
+      updateProgress(progress, playback.item.duration_ms, progressBar, timeElapsed);
+    }
+    screen.render();
+  }, 1000);
+
+  screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+    return process.exit(0);
+  });
+  screen.render();
+}
+
+function updateProgress(
+  progress: number,
+  duration: number,
+  progressBar: b.Widgets.ProgressBarElement,
+  timeText: b.Widgets.TextElement
+): void {
+  progressBar.setProgress((progress / duration) * 100);
+  timeText.setContent(msToTime(progress));
+}
+
 async function main(): Promise<void> {
   const spotify = new Spotify();
   await spotify.getToken();
   const playback = await spotify.getPlaybackState();
-  showScreen(playback);
+  // showScreen(playback);
+  showGrid(playback);
 }
 main().catch((err) => {
   console.log(err);
