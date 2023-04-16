a36b14bacd04e1e9b956e69d32cfd5cd7571a387
Kevin Lee <kevinlee3627@gmail.com>
2023-04-16 16:49:04 -0500

    Added formatting to search results

--- src/searchResultBox.ts
+++ src/searchResultBox.ts
@@ -1,6 +1,7 @@
 import * as b from 'blessed';
 import type bc from 'blessed-contrib';
 import type EventEmitter from 'events';
+import { cutoff, msToTime } from './main';
 import type { SearchType } from './spotify';
 import type { Album, Track } from './types';
 
@@ -57,15 +58,48 @@ export class SearchResultBox {
   }
 
   showAlbumResults(albums: Album[]): void {
-    this.element.setItems(albums.map((a) => a.name));
+    this.element.setItems(albums.map((a) => this.formatAlbumRow(a)));
     this.show();
   }
 
+  formatAlbumRow(album: Album): string {
+    const resultsWidth = (this.element.width as number) - 2;
+    const artistWidth = 50;
+    const releaseWidth = 10;
+    const durationWidth = 5;
+    // -3 for column gaps
+    const albumWidth = resultsWidth - artistWidth - releaseWidth - durationWidth - 3;
+
+    const albumName = cutoff(album.name, albumWidth).padEnd(albumWidth, ' ');
+    const artists = cutoff(
+      album.artists.map((a) => a.name).join(', '),
+      artistWidth
+    ).padEnd(artistWidth, ' ');
+    const release = cutoff(album.release_date, releaseWidth).padEnd(releaseWidth, ' ');
+    return `${albumName} ${artists} ${release}`;
+  }
+
   showTrackResults(tracks: Track[]): void {
-    this.element.setItems(
-      tracks.map((t) => `${t.name} by ${t.artists.map((a) => a.name).join(', ')}`)
-    );
+    this.element.setItems(tracks.map((t) => this.formatTrackRow(t)));
     this.show();
+  }
+
+  formatTrackRow(track: Track): string {
+    // -2 for borders,
+    const resultsWidth = (this.element.width as number) - 2;
+    const artistWidth = 50;
+    const albumWidth = 50;
+    const durationWidth = 5;
+    // -3 for column gaps
+    const songWidth = resultsWidth - artistWidth - albumWidth - durationWidth - 3;
+
+    const song = cutoff(track.name, songWidth).padEnd(songWidth, ' ');
+    const artists = cutoff(
+      track.artists.map((a) => a.name).join(', '),
+      albumWidth
+    ).padEnd(artistWidth, ' ');
+    const album = cutoff(track.album.name, albumWidth).padEnd(albumWidth, ' ');
+    return `${song} ${artists} ${album} ${msToTime(track.duration_ms)}`;
   }
 
   setResultType(type: SearchType): void {
