0b66ba86144754a16e95824569baf5c21a62058e
Kevin Lee <kevinlee3627@gmail.com>
2023-04-21 14:22:39 -0500

    Created more general trackBox

--- src/artistPage.ts
+++ src/artistPage.ts
@@ -1,9 +1,8 @@
-import * as b from 'blessed';
 import type bc from 'blessed-contrib';
 import type EventEmitter from 'events';
 import { Page } from './page';
+import { TrackBox } from './trackBox';
 import type { Artist, Track } from './types';
-import { cutoff, msToTime } from './util';
 
 interface ArtistPageOptions {
   gridWidth: number;
@@ -18,41 +17,34 @@ export class ArtistPage {
   customEmitter: EventEmitter;
   artist: Artist;
   page: Page;
-  topTracksBox: b.Widgets.ListElement;
+  topTracksBox: TrackBox;
 
   constructor(opts: ArtistPageOptions) {
     this.artist = opts.artist;
 
-    this.topTracksBox = opts.grid.set(0, opts.gridWidth / 2, 12, opts.gridWidth / 2, b.list, {
-      tags: true,
-      scrollable: true,
-      scrollbar: true,
-      noCellBorders: true,
-      interactive: true,
-      vi: true,
-      style: {
-        selected: { bg: 'red' },
-        scrollbar: { bg: 'blue' },
-        focus: { border: { fg: 'green' } },
-      },
-      keys: true,
+    this.topTracksBox = new TrackBox({
+      row: 0,
+      col: opts.gridWidth / 2,
+      width: opts.gridWidth / 2,
+      height: 12,
+      grid: opts.grid,
+      customEmitter: opts.customEmitter,
       label: 'top tracks',
+      tracks: opts.topTracks,
+      likedMapping: opts.topTracksLiked,
     });
 
-    this.topTracksBox.setItems(
-      opts.topTracks.map((t, i) => this.formatRow(t, i, opts.topTracksLiked[t.id]))
-    );
     this.page = new Page({
       name: 'artist',
       grid: opts.grid,
-      elements: [this.topTracksBox],
+      elements: [this.topTracksBox.element],
     });
 
     this.customEmitter = opts.customEmitter;
     this.customEmitter.on('artistPageHotkey', (key: string) => {
       switch (key) {
         case 't':
-          this.topTracksBox.focus();
+          this.topTracksBox.element.focus();
           break;
         case ':':
           this.customEmitter.emit('setActivePage', 'home');
@@ -61,28 +53,5 @@ export class ArtistPage {
           break;
       }
     });
-  }
-
-  formatRow(track: Track, index: number, liked: boolean): string {
-    const listWidth = this.topTracksBox.width as number;
-    const totalBorderWidth = 2;
-    const totalPaddingWidth = 3; // 4 columns, 1 col of padding between each column
-    const trackNumWidth = 2;
-    const durationWidth = 5;
-    const likedColWidth = 2;
-
-    const trackNameWidth =
-      listWidth -
-      totalBorderWidth -
-      totalPaddingWidth -
-      trackNumWidth -
-      durationWidth -
-      likedColWidth;
-    const trackNameCol = cutoff(track.name.padEnd(trackNameWidth, ' '), trackNameWidth);
-    const trackNumCol = String(index + 1).padEnd(trackNumWidth, ' ');
-    const durationCol = msToTime(track.duration_ms).padEnd(durationWidth, ' ');
-    const likedCol = liked ? '♥' : '';
-
-    return `${trackNumCol} ${trackNameCol} ${durationCol} ${likedCol}`;
   }
 }
---
+++ src/trackBox.ts
@@ -0,0 +1,92 @@
+import * as b from 'blessed';
+import type bc from 'blessed-contrib';
+import type EventEmitter from 'events';
+import { cutoff, msToTime } from './util';
+import type { Track } from './types';
+
+interface TrackBoxOptions {
+  row: number;
+  col: number;
+  width: number;
+  height: number;
+  grid: bc.Widgets.GridElement;
+  customEmitter: EventEmitter;
+  label: string;
+  tracks?: Track[];
+  likedMapping?: Record<string, boolean>;
+}
+
+export class TrackBox {
+  element: b.Widgets.ListElement;
+  customEmitter: EventEmitter;
+  selectedIndex: number = 0;
+  tracks: Track[] = [];
+
+  constructor(opts: TrackBoxOptions) {
+    this.element = opts.grid.set(opts.row, opts.col, opts.height, opts.width, b.list, {
+      tags: true,
+      scrollable: true,
+      scrollbar: true,
+      noCellBorders: true,
+      interactive: true,
+      vi: true,
+      style: {
+        selected: { bg: 'red' },
+        scrollbar: { bg: 'blue' },
+        focus: { border: { fg: 'green' } },
+      },
+      label: opts.label,
+      keys: true,
+    });
+
+    this.customEmitter = opts.customEmitter;
+
+    if (opts.tracks != null && opts.likedMapping != null) {
+      this.setTracks(opts.tracks);
+      this.updateList(opts.tracks, opts.likedMapping);
+    }
+
+    this.element.on('select', (item, i) => {
+      this.customEmitter.emit('playTrack', this.tracks[i].uri);
+    });
+  }
+
+  updateList(tracks: Track[], likedMapping: Record<string, boolean>): void {
+    const rows = tracks.map((track, i) => {
+      return this.formatRow(track, i, likedMapping[track.id]);
+    });
+    this.element.setItems(rows);
+  }
+
+  formatRow(track: Track, index: number, liked: boolean): string {
+    const listWidth = this.element.width as number;
+    const totalBorderWidth = 2;
+    const totalPaddingWidth = 3; // 4 columns, 1 col of padding between each column
+    const trackNumWidth = 2;
+    const durationWidth = 5;
+    const likedColWidth = 2;
+
+    const trackNameWidth =
+      listWidth -
+      totalBorderWidth -
+      totalPaddingWidth -
+      trackNumWidth -
+      durationWidth -
+      likedColWidth;
+
+    const trackNameCol = cutoff(track.name.padEnd(trackNameWidth, ' '), trackNameWidth);
+    const trackNumCol = String(index + 1).padEnd(trackNumWidth, ' ');
+    const durationCol = msToTime(track.duration_ms).padEnd(durationWidth, ' ');
+    const likedCol = liked ? '♥' : '';
+
+    return `${trackNumCol} ${trackNameCol} ${durationCol} ${likedCol}`;
+  }
+
+  setLabel(label: string): void {
+    this.element.setLabel(label);
+  }
+
+  setTracks(tracks: Track[]): void {
+    this.tracks = tracks;
+  }
+}
